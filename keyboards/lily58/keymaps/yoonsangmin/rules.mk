d116286773f1e9ec4b4cdef4ba3247cd0764d43d
yoonsangmin <dbstkdalsa@naver.com>
2023-03-17 00:26:19 +0900

    RGB 끄기

--- keyboards/lily58/keymaps/yoonsangmin/rules.mk
+++ keyboards/lily58/keymaps/yoonsangmin/rules.mk
@@ -5,6 +5,7 @@ CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGB_MATRIX_ENABLE = no
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
