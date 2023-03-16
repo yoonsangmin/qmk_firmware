5426706fddce31b3d21e0745966e0d6d46b81e56
yoonsangmin <dbstkdalsa@naver.com>
2023-03-16 23:14:54 +0900

    N키 롤오버, 양쪽 USB 사용 구현

--- keyboards/lily58/keymaps/yoonsangmin/config.h
+++ keyboards/lily58/keymaps/yoonsangmin/config.h
@@ -23,9 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
+#define EE_HANDS
 
 //#define TAPPING_FORCE_HOLD
 #define PERMISSIVE_HOLD
